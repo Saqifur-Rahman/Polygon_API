24f82b182cf4e27dba724e53f1bd84eddc33b0c2
Saqifur-Rahman <85213128+Saqifur-Rahman@users.noreply.github.com>
2021-06-24 17:54:30 +0530

    :white_check_mark: flask used for UI integration

--- main.py
+++ main.py
@@ -1,9 +1,64 @@
+import sys
+import os
+from flask import Flask, render_template, request, flash
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import requests
+from datetime import date
 
-def main():
+app = Flask(__name__)
+app.secret_key = "hello_world"
+
+def monthName(month):
+    month_name = {
+        "01": "January",
+        "02": "February",
+        "03": "March",
+        "04": "April",
+        "05": "May",
+        "06": "June",
+        "07": "July",
+        "08": "August",
+        "09": "September",
+        "10": "October",
+        "11": "November",
+        "12": "December"
+    }
+    return month_name[month]
+
+def monthRange(month, year):
+    month_range = {
+        "01": ["01","31"],
+        "03": ["01","31"],
+        "04": ["01","30"],
+        "05": ["01","31"],
+        "06": ["01","30"],
+        "07": ["01","31"],
+        "08": ["01","31"],
+        "09": ["01","30"],
+        "10": ["01","31"],
+        "11": ["01","30"],
+        "12": ["01","31"]
+    }
+
+    if int(year) % 4 == 0 and int(year) % 100 != 0:
+        month_range["02"] = ["01","29"]
+    elif int(year) % 100 == 0:
+        month_range["02"] = ["01","28"]
+    elif int(year) % 400 ==0:
+        month_range["02"] = ["01","29"]
+    else:
+        month_range["02"] = ["01","28"]
+
+    return month_range[str(month)]
+
+
+def plotChart(month, year):
+    month_range = monthRange(month, year)
+
     key = "9pq_4hMsbkqRCk9X7zgBlyuujMLgs6iA"
-    URL = f"https://api.polygon.io/v2/aggs/ticker/X:BTCUSD/range/1/day/2021-05-01/2021-05-10?adjusted=true&sort=asc&limit=120&apiKey={key}"
+    URL = f"https://api.polygon.io/v2/aggs/ticker/X:BTCUSD/range/1/day/{year}-{month}-{month_range[0]}/{year}-{month}-{month_range[1]}?adjusted=true&sort=asc&limit=120&apiKey={key}"
     r = requests.get(url = URL)
     data = r.json()
     date = []
@@ -12,18 +67,40 @@ def main():
 
     for i in range(data["resultsCount"]):
         result = data["results"][i]
-        date.append(i)
+        date.append(i+1)
         high.append(result["h"])
         low.append(result["l"])
 
+    if os.path.exists("static/plot.png"):
+        os.remove("static/plot.png")
+
     plt.plot(date, high, label = "highest price")
     plt.plot(date, low, label = "lowest price")
-    plt.xlabel("Days (May 2021)")
-    plt.ylabel("Price (USD)")
-    plt.title("Range of Prices of Bitcoin for 10 days")
+    plt.xlabel(f"Days [{monthName(month)} {year}]")
+    plt.ylabel("Price [USD]")
+    plt.title("Range of Prices of Bitcoin")
     plt.legend()
-    plt.savefig("plot.png")
+    plt.savefig("static/plot.png")
+    plt.clf()
+    return
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/result", methods=['POST'])
+def result():
+    if request.method == 'POST':
+        month = request.form['month']
+        year = request.form['year']
+
+        # invalid date
+        if int(month) > date.today().month:
+            flash("Data not available for this month.")
+            return render_template("home.html")
+
+        plotChart(month, year)
+        return render_template("result.html")
 
 if __name__ == '__main__':
-    main()
-
+    app.run(debug=True)
